4ccc49120121afab3345e4a42d5b3f477e7077e3
akasifislam <aseasifislam@gmail.com>
2024-12-25 20:59:00 +0600

    client setup

---
+++ backend/app/Http/Controllers/API/ApiAuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // return User::all();
+        // return "SFDs";
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
